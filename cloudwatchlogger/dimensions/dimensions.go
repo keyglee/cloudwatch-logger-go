117c0938cc4ce4b89da5c992092e597e97d81d02
Ryley Lamb <ryleylamb@gmail.com>
2024-07-10 16:51:18 -0700

    Making it so that the programmer can pass dimensions to be logged

--- cloudwatchlogger/dimensions/dimensions.go
+++ cloudwatchlogger/dimensions/dimensions.go
@@ -1,4 +1,4 @@
-package base
+package dimensions
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
--- cloudwatchlogger/misconfiguration.go
+++ cloudwatchlogger/misconfiguration.go
@@ -4,26 +4,40 @@ import (
 	"os"
 
 	"github.com/keyglee/cloudwatch-logger-go/cloudwatchlogger/base"
+	"github.com/keyglee/cloudwatch-logger-go/cloudwatchlogger/dimensions"
 	"github.com/keyglee/cloudwatch-logger-go/cloudwatchlogger/errors"
 	"github.com/keyglee/cloudwatch-logger-go/cloudwatchlogger/metrics"
 
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 )
 
-func LogMetric(metric_name metrics.Metrics, namespace string, accessed_resource string, config_error errors.ConfigError) (*cloudwatch.PutMetricDataOutput, error) {
+// LogMetric - Logs a metric to cloudwatch
+//
+// @param metric_name - Metric name to be logged
+//
+// @param namespace - Namespace of the metric being logged
+//
+// @param accessed_resource - Resource that is accessed by the metric being logged
+//
+// @param config_error - Error that occurred while accessing the resource. This is used for logging.
+//
+// @param extra_dimensions - Extra dimensions to be added to the metric being logged
+//
+// returns - cloudwatch.PutMetricDataOutput, error
+func LogMetric(metric_name metrics.Metrics, namespace string, accessed_resource string, config_error errors.ConfigError, extra_dimensions []*cloudwatch.Dimension) (*cloudwatch.PutMetricDataOutput, error) {
 
 	client := base.CloudwatchMetric{MetricName: string(metric_name), Namespace: namespace}
 
-	var dimensions []*cloudwatch.Dimension
+	var dimensionList []*cloudwatch.Dimension
 
-	dimensions = append(dimensions, base.CreateDimension("Error", string(config_error)))
-	dimensions = append(dimensions, base.CreateDimension("Service", os.Getenv("SERVICE_NAME")))
-	dimensions = append(dimensions, base.CreateDimension("Resource", accessed_resource))
-	dimensions = append(dimensions, base.CreateDimension("FunctionName", os.Getenv("AWS_LAMBDA_FUNCTION_NAME")))
-	dimensions = append(dimensions, base.CreateDimension("StackName", os.Getenv("STACK_NAME")))
-	dimensions = append(dimensions, base.CreateDimension("STAGE", os.Getenv("STAGE")))
+	dimensionList = append(dimensionList, dimensions.CreateDimension("Error", string(config_error)))
+	dimensionList = append(dimensionList, dimensions.CreateDimension("Resource", accessed_resource))
+	dimensionList = append(dimensionList, dimensions.CreateDimension("FunctionName", os.Getenv("AWS_LAMBDA_FUNCTION_NAME")))
+	dimensionList = append(dimensionList, dimensions.CreateDimension("STAGE", os.Getenv("STAGE")))
 
-	out, err := client.PutMetric(dimensions)
+	dimensionList = append(dimensionList, extra_dimensions...)
+
+	out, err := client.PutMetric(dimensionList)
 
 	return out, err
 }
